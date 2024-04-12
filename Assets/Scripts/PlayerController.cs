16796ea2b7d8a04a943a750402fa735372111640
Tontuu <81519031+Tontuu@users.noreply.github.com>
2024-04-12 09:50:12 -0300

    adiciona turning animations e reformulação na física wip

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,18 +4,27 @@ using System.Collections.Generic;
 using System.Runtime.CompilerServices;
 using UnityEngine;
 using UnityEngine.InputSystem;
+using System;
 
 [RequireComponent(typeof(Rigidbody2D))]
 public class PlayerController : MonoBehaviour
 {
     // Constants
-    const float runSpeed = 17.0f;
-    const float MAX_JUMP_VALUE = 35.0f;
+    const float horizontalJumpSpeed = 15.0f;
+    public float acceleration = 7f;
+    public float decceleration = 10f;
+    public float velPower = 0.9f;
+
+    [SerializeField]
+    private float runSpeed = 50.0f;
+    [SerializeField]
+    private float MAX_JUMP_VALUE = 35.0f;
 
     // Animation States
     private string currentState;
     const string PLAYER_IDLE = "player_idle";
     const string PLAYER_RUN = "player_run";
+    const string PLAYER_TURNING = "player_turning";
     const string PLAYER_JUMP = "player_jump";
     const string PLAYER_RISING = "player_rising";
     const string PLAYER_FALLING = "player_falling";
@@ -23,10 +32,13 @@ public class PlayerController : MonoBehaviour
 
     // Importants
     private bool jump = false;
-    public  float oldDir;
-    private float horizontalValue;
     private float jumpValue = 0.0f;
     Vector2 moveInput;
+
+    // Misc
+    private float oldDirOnJump;
+    public float dir;
+    public float oldDir;
 
     // Conditions
     private bool isCharging = false;
@@ -38,6 +50,8 @@ public class PlayerController : MonoBehaviour
     private bool isRising = false;
     private bool isFalling = false;
     private bool isFacingRight = true;
+    private bool isTurningDir = false;
+    public bool isHittingWall = false;
 
     // Unity items
     Rigidbody2D rb;
@@ -49,6 +63,8 @@ public class PlayerController : MonoBehaviour
     /// ================================================
     void Start()
     {
+        dir = 0;
+        oldDir = dir;
         rb = GetComponent<Rigidbody2D>();
         animator = GetComponent<Animator>();
         animator.Play("player_idle");
@@ -63,7 +79,9 @@ public class PlayerController : MonoBehaviour
 
     void HandleStates()
     {
-        if (isGrounded && !isRunning && !isCharging && !isJumping && !isLanded)
+        if (isGrounded && !isRunning &&
+            !isCharging && !isJumping &&
+            !isLanded && !isTurningDir)
         {
             ChangeAnimationState(PLAYER_IDLE);
         }
@@ -79,15 +97,19 @@ public class PlayerController : MonoBehaviour
         {
             ChangeAnimationState(PLAYER_LAND);
         }
-        else if (isRunning && !isJumping && !isCharging)
+        if (isTurningDir && isGrounded && !isJumping)
+        {
+            ChangeAnimationState(PLAYER_TURNING);
+        }
+        else if (isRunning && !isJumping && !isCharging && !isTurningDir)
         {
             ChangeAnimationState(PLAYER_RUN);
-        } 
-        else if (isRising) 
+        }
+        else if (isRising)
         {
             ChangeAnimationState(PLAYER_RISING);
         }
-        else if (isFalling) 
+        else if (isFalling)
         {
             ChangeAnimationState(PLAYER_FALLING);
         }
@@ -96,7 +118,9 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        horizontalValue = 0;
+        isHittingWall = false;
+        dir = 0;
+        oldDir = dir;
         {
             if (Input.GetKey(KeyCode.Space) && !isJumping)
             {
@@ -104,23 +128,41 @@ public class PlayerController : MonoBehaviour
                 jumpValue += 0.10f;
                 jumpValue = Mathf.Clamp(jumpValue, 4f, MAX_JUMP_VALUE);
                 isCharging = true;
-            } 
+            }
 
             if (Input.GetKeyUp(KeyCode.Space) || jumpValue > MAX_JUMP_VALUE)
             {
                 isCharged = true;
             }
 
-            if (isCharging == true && isCharged == true) 
+            if (isCharging == true && isCharged == true)
             {
                 jump = true;
             }
 
-            if (Input.GetKey(KeyCode.RightArrow) || Input.GetKey(KeyCode.LeftArrow)) {
-                horizontalValue = Input.GetAxisRaw("Horizontal");
-                if (isJumping) { oldDir = horizontalValue; }
+            if (Input.GetKey(KeyCode.RightArrow) || Input.GetKey(KeyCode.LeftArrow))
+            {
+                dir = Input.GetAxisRaw("Horizontal");
+                if (!isTurningDir && dir != 0)
+                {
+                    if (dir == -Math.Sign(rb.velocity.x))
+                    {
+                        if (Math.Abs(rb.velocity.x) > 6f)
+                        {
+                            isTurningDir = true;
+                            Invoke("ResetTurningDir", 0.2f);
+                        }
+                        else
+                        {
+                            setFacingDirection(dir);
+                        }
+                    }
+                }
+                if (isJumping) { oldDirOnJump = dir; }
                 isRunning = true;
-            } else {
+            }
+            else
+            {
                 isRunning = false;
             }
         }
@@ -129,14 +171,27 @@ public class PlayerController : MonoBehaviour
         {
             isFalling = true;
             isRising = false;
-        } else if (rb.velocity.y > 5f)
+        }
+        else if (rb.velocity.y > 5f)
         {
             isFalling = false;
             isRising = true;
-        } else {
+        }
+        else
+        {
             isFalling = false;
             isRising = false;
         }
+
+
+        if (isRunning)
+        {
+            if (!isCharging && isGrounded && !isTurningDir)
+            {
+                setFacingDirection(dir);
+            }
+        }
+
 
         // Get run input
         HandleStates();
@@ -144,8 +199,10 @@ public class PlayerController : MonoBehaviour
     void FixedUpdate()
     {
         OnLand();
-        if (jump) {
-            rb.velocity = new Vector2(horizontalValue * runSpeed, jumpValue);
+        if (jump)
+        {
+            setFacingDirection(dir);
+            rb.velocity = new Vector2(dir * horizontalJumpSpeed, jumpValue);
             isCharging = false;
             isCharged = false;
             jump = false;
@@ -174,50 +231,67 @@ public class PlayerController : MonoBehaviour
             // Cannot run either while charging or not on ground
             if (!isCharging && isGrounded)
             {
-                rb.velocity = new Vector2(horizontalValue * runSpeed, rb.velocity.y);
-                setFacingDirection(horizontalValue);
+                float targetSpeed = dir * runSpeed;
+                float speedDif = targetSpeed - rb.velocity.x;
+                float accelRate = (Mathf.Abs(targetSpeed) > 0.01f) ? acceleration : decceleration;
+                float movement = Mathf.Pow(Mathf.Abs(speedDif) * accelRate, velPower) * Mathf.Sign(speedDif);
+                rb.AddForce(movement * Vector2.right);
+                // rb.velocity = new Vector2(dir * runSpeed, rb.velocity.y);
             }
+
+            if (isGrounded && dir == 0)
+            {
+            }
+
         }
         else
         {
             // Only lose the velocity on the ground, not on the air (falling)
-            if (isGrounded)
+            if (isGrounded && dir == 0)
             {
-                rb.velocity = new Vector2(0, rb.velocity.y);
+                // rb.velocity = new Vector2(rb.velocity.x * 0.2f, rb.velocity.y);
             }
         }
     }
+
+    void ResetTurningDir()
+    {
+        isTurningDir = false;
+        if (!isJumping)
+            setFacingDirection(dir);
+    }
+
     void ResetJump()
     {
-        oldDir = 0;
+        oldDirOnJump = 0;
         isCharging = false;
         jumpValue = 0f;
     }
-    private void setFacingDirection(float horizontalValue)
+    private void setFacingDirection(float dir)
     {
-        if (horizontalValue > 0 && !isFacingRight)
+        if (dir > 0.0 && !isFacingRight)
         {
             GetComponent<SpriteRenderer>().flipX = false;
             isFacingRight = true;
         }
-        else if (horizontalValue < 0 && isFacingRight)
+        else if (dir < 0.0 && isFacingRight)
         {
             GetComponent<SpriteRenderer>().flipX = true;
             isFacingRight = false;
         }
     }
 
-    void OnWallHit() 
+    void OnWallHit()
     {
         // Invert player
         rb.velocity = new Vector2((rb.velocity.x * -1) / 2.5f, rb.velocity.y / 1.25f);
 
-        if (oldDir < 0 && !isFacingRight)
+        if (oldDirOnJump < 0.0 && !isFacingRight)
         {
             GetComponent<SpriteRenderer>().flipX = false;
             isFacingRight = true;
         }
-        else if (oldDir > 0 && isFacingRight)
+        else if (oldDirOnJump > 0.0 && isFacingRight)
         {
             GetComponent<SpriteRenderer>().flipX = true;
             isFacingRight = false;
@@ -226,12 +300,12 @@ public class PlayerController : MonoBehaviour
 
     private void OnTriggerEnter2D(Collider2D other)
     {
-        if (other.gameObject.CompareTag("Wall")) 
+        if (other.gameObject.CompareTag("Wall"))
         {
             OnWallHit();
         }
 
-        
+
         if (other.gameObject.CompareTag("Ground"))
         {
             isGrounded = true;
