6127e9c9d04736d21232fec652912f779b48c556
Tontuu <81519031+Tontuu@users.noreply.github.com>
2024-05-03 14:28:03 -0300

    fix gravity on jumping

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,6 +1,4 @@
 using System.ComponentModel;
-using System.Collections;
-using System.Collections.Generic;
 using System.Runtime.CompilerServices;
 using UnityEngine;
 using UnityEngine.InputSystem;
@@ -11,20 +9,16 @@ public class PlayerController : MonoBehaviour
 {
     // Constants
     const float horizontalJumpSpeed = 15.0f;
-    public float acceleration = 7f;
-    public float decceleration = 10f;
-    public float velPower = 0.9f;
-
-    [SerializeField]
-    private float runSpeed = 50.0f;
-    [SerializeField]
-    private float MAX_JUMP_VALUE = 35.0f;
+    const float runSpeed = 18.0f;
+    const float MAX_JUMP_MAGNITUDE = 52.0f;
+    const float JUMP_CHARGE_MAGNITUDE = 0.40f;
+    const float MIN_CHARGE_MAGNITUDE = 10.0f;
+    const float PLAYER_GRAVITY = 7.0f;
 
     // Animation States
     private string currentState;
     const string PLAYER_IDLE = "player_idle";
     const string PLAYER_RUN = "player_run";
-    const string PLAYER_TURNING = "player_turning";
     const string PLAYER_JUMP = "player_jump";
     const string PLAYER_RISING = "player_rising";
     const string PLAYER_FALLING = "player_falling";
@@ -32,13 +26,13 @@ public class PlayerController : MonoBehaviour
 
     // Importants
     private bool jump = false;
-    private float jumpValue = 0.0f;
+    public float jumpValue = 0.0f;
     Vector2 moveInput;
 
     // Misc
     private float oldDirOnJump;
-    public float dir;
-    public float oldDir;
+    private float dir;
+    private float oldDir;
 
     // Conditions
     private bool isCharging = false;
@@ -50,8 +44,6 @@ public class PlayerController : MonoBehaviour
     private bool isRising = false;
     private bool isFalling = false;
     private bool isFacingRight = true;
-    private bool isTurningDir = false;
-    public bool isHittingWall = false;
 
     // Unity items
     Rigidbody2D rb;
@@ -66,6 +58,7 @@ public class PlayerController : MonoBehaviour
         dir = 0;
         oldDir = dir;
         rb = GetComponent<Rigidbody2D>();
+        rb.gravityScale = PLAYER_GRAVITY;
         animator = GetComponent<Animator>();
         animator.Play("player_idle");
     }
@@ -81,7 +74,7 @@ public class PlayerController : MonoBehaviour
     {
         if (isGrounded && !isRunning &&
             !isCharging && !isJumping &&
-            !isLanded && !isTurningDir)
+            !isLanded)
         {
             ChangeAnimationState(PLAYER_IDLE);
         }
@@ -97,11 +90,7 @@ public class PlayerController : MonoBehaviour
         {
             ChangeAnimationState(PLAYER_LAND);
         }
-        if (isTurningDir && isGrounded && !isJumping)
-        {
-            ChangeAnimationState(PLAYER_TURNING);
-        }
-        else if (isRunning && !isJumping && !isCharging && !isTurningDir)
+        else if (isRunning && !isJumping && !isCharging)
         {
             ChangeAnimationState(PLAYER_RUN);
         }
@@ -118,19 +107,18 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        isHittingWall = false;
         dir = 0;
         oldDir = dir;
         {
             if (Input.GetKey(KeyCode.Space) && !isJumping)
             {
                 rb.velocity = new Vector2(0, 0);
-                jumpValue += 0.10f;
-                jumpValue = Mathf.Clamp(jumpValue, 4f, MAX_JUMP_VALUE);
+                jumpValue += JUMP_CHARGE_MAGNITUDE;
+                jumpValue = Mathf.Clamp(jumpValue, MIN_CHARGE_MAGNITUDE, MAX_JUMP_MAGNITUDE);
                 isCharging = true;
             }
 
-            if (Input.GetKeyUp(KeyCode.Space) || jumpValue > MAX_JUMP_VALUE)
+            if (jumpValue >= MAX_JUMP_MAGNITUDE || Input.GetKeyUp(KeyCode.Space))
             {
                 isCharged = true;
             }
@@ -142,24 +130,20 @@ public class PlayerController : MonoBehaviour
 
             if (Input.GetKey(KeyCode.RightArrow) || Input.GetKey(KeyCode.LeftArrow))
             {
-                dir = Input.GetAxisRaw("Horizontal");
-                if (!isTurningDir && dir != 0)
+                // Don't move if both keys are being pressed.
+                if (!(Input.GetKey(KeyCode.RightArrow) && Input.GetKey(KeyCode.LeftArrow)))
                 {
-                    if (dir == -Math.Sign(rb.velocity.x))
+                    dir = Input.GetAxisRaw("Horizontal");
+                    if (dir != 0)
                     {
-                        if (Math.Abs(rb.velocity.x) > 6f)
-                        {
-                            isTurningDir = true;
-                            Invoke("ResetTurningDir", 0.2f);
-                        }
-                        else
+                        if (dir == -Math.Sign(rb.velocity.x))
                         {
                             setFacingDirection(dir);
                         }
                     }
+                    if (isJumping) { oldDirOnJump = dir; }
+                    isRunning = true;
                 }
-                if (isJumping) { oldDirOnJump = dir; }
-                isRunning = true;
             }
             else
             {
@@ -172,7 +156,7 @@ public class PlayerController : MonoBehaviour
             isFalling = true;
             isRising = false;
         }
-        else if (rb.velocity.y > 5f)
+        else if (rb.velocity.y > 1.0f)
         {
             isFalling = false;
             isRising = true;
@@ -186,7 +170,7 @@ public class PlayerController : MonoBehaviour
 
         if (isRunning)
         {
-            if (!isCharging && isGrounded && !isTurningDir)
+            if (!isCharging && isGrounded)
             {
                 setFacingDirection(dir);
             }
@@ -223,6 +207,10 @@ public class PlayerController : MonoBehaviour
     {
         if (isRunning)
         {
+            if (isRunning && dir == 0)
+            {
+                isRunning = false;
+            }
             if (isCharging)
             {
                 isRunning = false;
@@ -231,12 +219,7 @@ public class PlayerController : MonoBehaviour
             // Cannot run either while charging or not on ground
             if (!isCharging && isGrounded)
             {
-                float targetSpeed = dir * runSpeed;
-                float speedDif = targetSpeed - rb.velocity.x;
-                float accelRate = (Mathf.Abs(targetSpeed) > 0.01f) ? acceleration : decceleration;
-                float movement = Mathf.Pow(Mathf.Abs(speedDif) * accelRate, velPower) * Mathf.Sign(speedDif);
-                rb.AddForce(movement * Vector2.right);
-                // rb.velocity = new Vector2(dir * runSpeed, rb.velocity.y);
+                rb.velocity = new Vector2(dir * runSpeed, rb.velocity.y);
             }
 
             if (isGrounded && dir == 0)
@@ -249,16 +232,9 @@ public class PlayerController : MonoBehaviour
             // Only lose the velocity on the ground, not on the air (falling)
             if (isGrounded && dir == 0)
             {
-                // rb.velocity = new Vector2(rb.velocity.x * 0.2f, rb.velocity.y);
+                rb.velocity = new Vector2(rb.velocity.x * 0.2f, rb.velocity.y);
             }
         }
-    }
-
-    void ResetTurningDir()
-    {
-        isTurningDir = false;
-        if (!isJumping)
-            setFacingDirection(dir);
     }
 
     void ResetJump()
@@ -269,15 +245,18 @@ public class PlayerController : MonoBehaviour
     }
     private void setFacingDirection(float dir)
     {
-        if (dir > 0.0 && !isFacingRight)
+        if (!isJumping)
         {
-            GetComponent<SpriteRenderer>().flipX = false;
-            isFacingRight = true;
-        }
-        else if (dir < 0.0 && isFacingRight)
-        {
-            GetComponent<SpriteRenderer>().flipX = true;
-            isFacingRight = false;
+            if (dir > 0.0 && !isFacingRight)
+            {
+                GetComponent<SpriteRenderer>().flipX = false;
+                isFacingRight = true;
+            }
+            else if (dir < 0.0 && isFacingRight)
+            {
+                GetComponent<SpriteRenderer>().flipX = true;
+                isFacingRight = false;
+            }
         }
     }
 
